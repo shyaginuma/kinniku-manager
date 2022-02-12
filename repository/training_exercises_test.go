3f0744e78f8cb6504e7e66d873308890f5a95fce
Shinya Yaginuma <yaginuuun@gmail.com>
2022-02-12 11:37:31 +0900

    Write test of TrainingExcerciseRepository.ReadAll() (#2)
    * Use go-sqlmock
    
    * pass test
    
    * setup github actions

---
+++ repository/training_exercises_test.go
@@ -0,0 +1,41 @@
+package repository
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/kinniku-manager/model"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func TestTrainingExcerciseRepository_ReadAll(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Error(err.Error())
+	}
+	defer db.Close()
+
+	returnRows := sqlmock.NewRows([]string{"id", "name", "description", "target", "category", "difficulty"})
+	returnRows.AddRow("1", "Barbell Curl", "Barbell Curl", "biceps", "barbell", "beginner")
+	mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM training_exercises`)).WillReturnRows(returnRows)
+
+	repository := &TrainingExcerciseRepository{Database: db}
+	excercises, err := repository.ReadAll()
+	if err != nil {
+		t.Error(err.Error())
+	}
+	expected_response := []model.TrainingExcercise{}
+	data := model.TrainingExcercise{
+		ID:          "1",
+		Name:        "Barbell Curl",
+		Description: "Barbell Curl",
+		Target:      model.Biceps,
+		Category:    model.Barbell,
+		Difficulty:  model.Beginner,
+	}
+	expected_response = append(expected_response, data)
+	assert.Equal(t, expected_response, excercises)
+}
