a9fdf5f3d05bc0b3483541db405f3cc9ee39f451
shinya-yaginuma <yaginuuun@gmail.com>
2022-03-01 10:04:15 +0900

    Fix indent

--- mysql/init_db.sql
+++ mysql/init_db.sql
@@ -1,6 +1,6 @@
 create database if not exists trainings;
 create table if not exists trainings.training_exercises(
-    id          INT AUTO_INCREMENT not null,
+	id          INT AUTO_INCREMENT not null,
 	name        VARCHAR(128)       not null,
 	description VARCHAR(255)       not null,
 	target      VARCHAR(128)       not null,
