a4fa79e2cc61fa472b1f95b51da8fc41560b5904
shinya-yaginuma <yaginuuun@gmail.com>
2022-03-15 09:58:50 +0900

    Create skelton

--- repository/training_menu.go
+++ repository/training_menu.go
@@ -1,1 +1,31 @@
 package repository
+
+import (
+	"database/sql"
+
+	"github.com/kinniku-manager/model"
+)
+
+type TrainingMenuRepository struct {
+	Database *sql.DB
+}
+
+func (repository TrainingMenuRepository) ReadAll() ([]model.TrainingMenu, error) {
+	return []model.TrainingMenu{}, nil
+}
+
+func (repository TrainingMenuRepository) Read(id int64) (model.TrainingMenu, error) {
+	return model.TrainingMenu{}, nil
+}
+
+func (repository TrainingMenuRepository) Create(newTraningMenu model.TrainingMenu) error {
+	return nil
+}
+
+func (repository TrainingMenuRepository) Update(modifiedTraningMenu model.TrainingMenu) error {
+	return nil
+}
+
+func (repository TrainingMenuRepository) Delete(id int64) error {
+	return nil
+}
---
+++ repository/training_menu_test.go
@@ -0,0 +1,23 @@
+package repository
+
+import "testing"
+
+func TestTrainingMenuRepository_ReadAll(t *testing.T) {
+
+}
+
+func TestTrainingMenuRepository_Read(t *testing.T) {
+
+}
+
+func TestTrainingMenuRepository_Create(t *testing.T) {
+
+}
+
+func TestTrainingMenuRepository_Update(t *testing.T) {
+
+}
+
+func TestTrainingMenuRepository_Delete(t *testing.T) {
+
+}
